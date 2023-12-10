edc2aad29f2ebc44432ab4296afd306e72af217b
WalidKAD <walidkadich@gmail.com>
2023-12-10 18:28:21 +0100

    add sign out functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
-import { errorHandler } from "../Utils/error.js";
+import { errorHandler } from "../utils/error.js";
 import jwt from "jsonwebtoken";
 
 export const signUp = async (req, res, next) => {
@@ -61,6 +61,15 @@ export const google = async (req, res, next) => {
         .status(200)
         .json(rest);
     }
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signOut = async (req, res, next) => {
+  try {
+    res.clearCookie("access_token");
+    res.status(200).json("User has been logged Out!");
   } catch (error) {
     next(error);
   }
