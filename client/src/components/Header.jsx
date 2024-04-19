81c829fa48df7708a883ca3f44835a7675f1f26e
WalidKAD <walidkadich@gmail.com>
2024-04-19 04:41:58 +0100

    complete header search from functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,41 +1,71 @@
-import {FaSearch} from 'react-icons/fa';
-import { Link } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { FaSearch } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function Header() {
-  const {currentUser} = useSelector(state => state.user)
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
-    <header className='bg-state-200 shadow-md'>
-      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-      <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-slate-500'>Vald</span>
-            <span className='text-slate-700'>Estate</span>
-        </h1>
-        <form className='bg-state-100 p-3 rounded-lg flex items-center'>
-          <input 
-          type='text'
-          placeholder='search... '
-          className='bg-transparent focus:outline-none w-4 sm:w-64 '
-          /> 
-          <FaSearch className='text-state-600' />
+    <header className="bg-state-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-slate-500">Vald</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
+        <form
+          onSubmit={handleSubmit}
+          className="bg-state-100 p-3 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="search... "
+            className="bg-transparent focus:outline-none w-4 sm:w-64 "
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button>
+            <FaSearch className="text-state-600" />
+          </button>
         </form>
-        <ul className='flex gap-4'>
-          <Link to='/'>
-          <li className='hidden sm:inline text-slate-700 hover:underline'>Home</li>
+        <ul className="flex gap-4">
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
           </Link>
-
-          <Link to='/about'>
-          <li className='hidden sm:inline text-slate-700 hover:underline'>About</li>
+          <Link to="/about">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
           </Link>
-
-
-          <Link to='/profile'>
-
-          {currentUser ? (
-            <img className='rouded-full h-7 w-7 object-cover' src={currentUser.avatar} alt="profile" />
-          ): (
-          <li className=' text-slate-700 hover:underline'>Sign in</li> 
-          )}
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                className="rouded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="profile"
+              />
+            ) : (
+              <li className=" text-slate-700 hover:underline">Sign in</li>
+            )}
           </Link>
         </ul>
       </div>
